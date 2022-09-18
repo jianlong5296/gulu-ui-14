dfbe4f138bb423766eafb0c0ccee1371aa801b0d
edward <2219080450@qq.com>
2022-09-19 01:17:11 +0800

    Topnav 组件封装完毕

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const router = createRouter({
     history:history,
     routes:[
         {path:'/', component:Home},  //易错写为components
-        {path:'/', component:Doc}
+        {path:'/doc', component:Doc}
     ]
 })
 
