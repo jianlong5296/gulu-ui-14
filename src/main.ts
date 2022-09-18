e6c6274dcbc2be90a3cd17eed77ec13c0ac30df7
edward <2219080450@qq.com>
2022-09-19 01:06:20 +0800

    搭建 Html

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
-import Frank from './components/Frank.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history:history,
     routes:[
-        {path:'/', component:Frank}
+        {path:'/', component:Home},  //易错写为components
+        {path:'/', component:Doc}
     ]
 })
 
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <div class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </div>
+    </div>
+  </div>
+</template>
