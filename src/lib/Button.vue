456c923390cbf3e9234f481a8a7363997c7fe4cc
edward <2219080450@qq.com>
2022-09-20 11:15:17 +0800

    创建 Button 组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button>
-    <slot />
-  </button>
+  <div>
+    <button>
+      <slot />
+    </button>
+  </div>
 </template>
 <script lang="ts">
-export default {};
+export default {
+  inheritAttrs: false, //不再继承默认属性
+  props: {},
+  setup(props, context) {
+    const { size, ...rest } = context.attrs;
+    return { size, rest };
+  },
+};
 </script>
