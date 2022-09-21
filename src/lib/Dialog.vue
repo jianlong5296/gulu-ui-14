38e8a81037d155ca7c7756af2672ddebd6233e7e
edward <2219080450@qq.com>
2022-09-21 20:19:41 +0800

    Dialog 支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,17 +1,19 @@
 <template>
   <template v-if="visible">
-    <div class="gulu-dialog-overlay"></div>
+    <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
     <div class="gulu-dialog-wrapper">
       <div class="gulu-dialog">
-        <header>标题 <span class="gulu-dialog-close"></span></header>
+        <header>
+          标题 <span @click="close" class="gulu-dialog-close"></span>
+        </header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
           <!-- 引入组件时，button要大写，易错 -->
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -25,8 +27,43 @@ export default {
       type: Boolean,
       default: false,
     },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
+    },
   },
   components: { Button },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        //完整写法为 (props.ok && props.ok() !== false)
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit("cancel");
+    };
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel,
+    };
+  },
 };
 </script>
 <style lang="scss">
