8a7bc8641442599f426609167baa47da8fc66e2d
edward <2219080450@qq.com>
2022-09-24 13:49:56 +0800

    遇到 vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" :is="current" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
 import Tab from "./Tab.vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -15,13 +33,56 @@ export default {
         throw new Error("Tabs 子标签必须为 Tab");
       }
     });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
       titles,
+      current,
+      select,
     };
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
